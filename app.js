3ec001dbdb5b0c34c267ecefb512e2d5fd97e035
Catherine Wheeler-Mushlin <107967088+CatW-M@users.noreply.github.com>
2024-02-10 11:31:23 -0700

    Feat: Added Data Sanitization

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -12,7 +14,7 @@ const app = express();
 
 // 1) GLOBAL MIDDLEWARES
 // SET Security HTTP Headers
-app.use(helmet())
+app.use(helmet());
 
 // Development Logging
 
@@ -31,9 +33,18 @@ app.use("/api", limiter);
 
 // Body Parser, Reading data from body into req.body
 
-app.use(express.json({
-  limit: '10kb'
-}));
+app.use(
+  express.json({
+    limit: "10kb",
+  }),
+);
+
+// Data Sanitization against NOSQL query injection
+app.use(mongoSanitize());
+
+// Data Sanitization against XSS
+app.use(xss());
+
 // Serving static files
 app.use(express.static("./public"));
 
